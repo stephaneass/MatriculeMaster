784b2e07e69daf570d08e722152132f9fecc1aa0
Stephane ASSOCLE <mensste30@gmail.com>
2023-08-17 23:20:23 +0100

    users table review

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,18 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('first_name', 60);
+            $table->string('last_name', 60);
+            $table->string('gender', 1);
+            $table->string('matricule_number', 20);
+            $table->date('birth_date');
+            $table->date('registration_date');
+            $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
+            $table->engine = 'InnoDB';
         });
     }
 

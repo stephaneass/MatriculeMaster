751b3fc0e8cbbe273214f0b57b811dd81d7fbf14
Stephane ASSOCLE <mensste30@gmail.com>
2023-08-18 11:01:07 +0100

    spatie permission

--- resources/views/auth/layout.blade.php
+++ resources/views/auth/layout.blade.php
@@ -71,7 +71,7 @@
                     <div class="col-lg-12">
                         <div class="text-center">
                             <p class="mb-0 text-muted">&copy;
-                                <script>document.write(new Date().getFullYear())</script> Velzon. Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesbrand
+                                <script>document.write(new Date().getFullYear())</script> Matricule Master. Crafted with <i class="mdi mdi-heart text-danger"></i> by OrisHas
                             </p>
                         </div>
                     </div>
